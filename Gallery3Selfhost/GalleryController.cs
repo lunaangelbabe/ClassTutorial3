42736e82598d2c60a55e17aef3ace11ff8c8aa0e
lunaangelbabe <beccastephens1992@gmail.com>
2017-05-15 12:44:20 +1200

    AHHH PUSH IT!

---
+++ Gallery3Selfhost/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3Selfhost
+{
+    public class GalleryController : System.Web.Http.ApiController
+    {
+    }
+}
