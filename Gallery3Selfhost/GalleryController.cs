94925a674544f262703009d9cd6bffd9ec2490c8
lunaangelbabe <beccastephens1992@gmail.com>
2017-05-15 14:19:15 +1200

    PUSH IT GOOOD!!!!

--- Gallery3Selfhost/GalleryController.cs
+++ Gallery3Selfhost/GalleryController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace Gallery3Selfhost
 {
     public class GalleryController : System.Web.Http.ApiController
     {
+        public List<string> GetArtistNames()
+        {
+            DataTable lcResult = clsDbConnection.GetDataTable("SELECT Name FROM Artist", null);
+            List<string> lcNames = new List<string>();
+            foreach (DataRow dr in lcResult.Rows)
+                lcNames.Add((string)dr[0]);
+            return lcNames;
+        }
     }
 }
---
+++ Gallery3WinForm/ServiceClient.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3WinForm
+{
+    public static class ServiceClient
+    {
+        internal async static Task<List<string>> GetArtistNamesAsync()
+        {
+            using (HttpClient lcHttpClient = new HttpClient())
+                return JsonConvert.DeserializeObject<List<string>>(await lcHttpClient.GetStringAsync("http://localhost:60064/api/gallery/GetArtistNames/"));
+        }
+    }
+}
